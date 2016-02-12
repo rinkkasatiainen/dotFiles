d23890c69fa16d091f049fd8d3d06d9d5d13a363
Aki Salmi <aki.salmi@ambientia.fi>
2016-02-12 14:34:09 +0200

    removed GIT shortcuts - might be useful later

--- fish/functions/gitv.fish
+++
@@ -1,3 +0,0 @@
-function gitv -d "Show git log using Gitv in vim"
-  command vim (git rev-parse --show-toplevel)/.git/index -c "Gitv $args" -c "tabonly"
-end
--- fish/functions/grt.fish
+++
@@ -1,3 +0,0 @@
-function grt -d "Change to top of the current repository or submodule"
-  cd (git rev-parse --show-toplevel; or echo ".") 
-end
